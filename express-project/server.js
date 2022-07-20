38612f87fa3a4defe4e33ffc27b50c5ffc177003
Matt Cook <mathew.cook@mastery.net>
2022-07-20 09:29:09 -0500

    Playing around with params

--- express-project/server.js
+++ express-project/server.js
@@ -1,34 +1,55 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const friendsRouter = require('./routes/friends.router');
-const messagesRouter = require('./routes/messages.router');
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 const PORT = 3000;
 
-app.use((req, res, next) => {
-  const start = Date.now();
-  next();
-  const delta = Date.now() - start;
-  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
+// app.use((req, res, next) => {
+//   const start = Date.now();
+//   next();
+//   const delta = Date.now() - start;
+//   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
+// });
+
+// app.use("/site", express.static(path.join(__dirname, "public")));
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.render("index", {
+//     title: "My Friends Are VERYY Clever",
+//     caption: "Let's go skiing!",
+//   });
+// });
+
+const friends = [
+  { id: 0, name: "Flynn" },
+  { id: 1, name: "Danelle" },
+];
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
 });
 
-app.use('/site', express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'My Friends Are VERYY Clever',
-    caption: 'Let\'s go skiing!',
-  });
+app.get("/friends/:id", (req, res) => {
+  const friendId = Number(req.params.id);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist",
+    });
+  }
 });
-app.use('/friends', friendsRouter);
-app.use('/messages', messagesRouter);
+// app.use("/friends", friendsRouter);
+// app.use("/messages", messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
