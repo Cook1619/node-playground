fa7db835db81e70efdadee631fff9d88ba95bccb
Matt Cook <mathew.cook@mastery.net>
2022-07-27 15:04:56 -0500

    server up up with schema

--- project-managment-app/server/index.js
+++ project-managment-app/server/index.js
@@ -1,11 +1,25 @@
-import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+const express = require("express");
+// const colors = require('colors');
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+// const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Connect to database
+// connectDB();
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
