dcd5939600b7428fb9286acdcb7743346ece647d
Matt Cook <mathew.cook@mastery.net>
2022-07-29 16:12:47 -0500

    Adding getTaskById route

--- nest-fun/src/tasks/tasks.service.ts
+++ nest-fun/src/tasks/tasks.service.ts
@@ -10,6 +10,11 @@ export class TasksService {
   getAllTasks() {
     return this.tasks;
   }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
