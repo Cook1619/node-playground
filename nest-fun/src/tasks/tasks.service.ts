1d1aabb42d17d4b583e889b7520314593046a3e7
Matt Cook <mathew.cook@mastery.net>
2022-07-29 15:48:26 -0500

    setting up first 2 endpoints

--- nest-fun/src/tasks/tasks.service.ts
+++ nest-fun/src/tasks/tasks.service.ts
@@ -1,4 +1,22 @@
+import { Task, TaskStatus } from './task.model';
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
