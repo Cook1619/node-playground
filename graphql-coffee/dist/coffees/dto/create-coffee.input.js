8325ea85de2367b51673c513f80e4715ab73377c
Matt Cook <mathew.cook@mastery.net>
2022-08-26 10:58:07 -0500

    Setting up validation on input

--- graphql-coffee/dist/coffees/dto/create-coffee.input.js
+++ graphql-coffee/dist/coffees/dto/create-coffee.input.js
@@ -5,14 +5,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CreateCoffeeInput = void 0;
 const graphql_1 = require("@nestjs/graphql");
+const class_validator_1 = require("class-validator");
 let CreateCoffeeInput = class CreateCoffeeInput {
     static _GRAPHQL_METADATA_FACTORY() {
         return { name: { type: () => String }, brand: { type: () => String }, flavors: { type: () => [String] } };
     }
 };
+__decorate([
+    (0, class_validator_1.MinLength)(3),
+    (0, graphql_1.Field)(() => String, { description: 'A new coffee name' }),
+    __metadata("design:type", String)
+], CreateCoffeeInput.prototype, "name", void 0);
 CreateCoffeeInput = __decorate([
     (0, graphql_1.InputType)()
 ], CreateCoffeeInput);
--- graphql-coffee/src/coffees/dto/create-coffee.input.ts
+++ graphql-coffee/src/coffees/dto/create-coffee.input.ts
@@ -1,7 +1,10 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 
 @InputType()
 export class CreateCoffeeInput {
+  @MinLength(3)
+  @Field(() => String, { description: 'A new coffee name' })
   name: string;
   brand: string;
   flavors: string[];
