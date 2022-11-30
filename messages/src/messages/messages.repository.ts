24d0ac5a75c974537475728d66a166ed35243e64
Matt Cook <mathew.cook@mastery.net>
2022-11-29 19:55:47 -0600

    setting up writing and readingot file for dummy db

--- messages/src/messages/messages.repository.ts
+++ messages/src/messages/messages.repository.ts
@@ -1,4 +1,5 @@
 import {readFile, writeFile } from 'fs/promises'
+import { v4 as uuid } from 'uuid';
 
 export class MessagesRepository {
     async findOne(id: string){
@@ -6,6 +7,17 @@ export class MessagesRepository {
         const messages = JSON.parse(contents);
         return messages[id]
     }
-    async findAll(){}
-    async create(message: string){}
+    async findAll(){
+        const contents = await readFile('messages.json', 'utf8');
+        return JSON.parse(contents);
+    }
+    async create(content: string){
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+        
+        const id = uuid();
+        messages[id] = {id, content}
+
+        await writeFile('messages.json', JSON.stringify(messages))
+    }
 }
