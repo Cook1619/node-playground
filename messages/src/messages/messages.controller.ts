bd5f6446e77b30029d4b1c1716b7ab8ab7a39441
Matt Cook <mathew.cook@mastery.net>
2022-11-29 20:06:37 -0600

    full working example

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,18 +1,25 @@
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { MessagesService } from './messages.service';
 
 @Controller('/messages')
 export class MessagesController {
+  messagesService: MessagesService
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll()
+  }
 
   @Post()
   createMessage(@Body() body:CreateMessageDto) {
-    console.log(body)
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id:string) {
-    console.log(id)
+    return this.messagesService.findOne(id);
   }
 }
