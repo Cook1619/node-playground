1dc83560c22066ee871d9eb8a8acf4b60dceb1df
Matt Cook <mathew.cook@mastery.net>
2022-12-01 20:00:26 -0600

    setting up user sign up

--- car-value-api/src/users/users.controller.ts
+++ car-value-api/src/users/users.controller.ts
@@ -1,11 +1,13 @@
+import { UsersService } from './users.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private usersService: UsersService) { }
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        console.log('body--', body)
+        return this.usersService.create(body.email, body.password)
     }
 
 }
