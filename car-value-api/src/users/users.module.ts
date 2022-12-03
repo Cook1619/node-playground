39061e5f68742b87813b8d5580cc97c6da3e4243
Matt Cook <mathew.cook@mastery.net>
2022-12-03 17:23:32 -0600

    Setting up auth salt/hashing pw

--- car-value-api/src/users/users.module.ts
+++ car-value-api/src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Module({
   // Creates repo for us for free by creating entity file
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule { }
