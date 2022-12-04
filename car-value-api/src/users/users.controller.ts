1e8761c5b5e134955addffed9952554e9fce68cb
Matt Cook <mathew.cook@mastery.net>
2022-12-03 19:41:56 -0600

    setting up signing in and out

--- car-value-api/src/users/users.controller.ts
+++ car-value-api/src/users/users.controller.ts
@@ -24,6 +24,17 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 @Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService, private authService: AuthService) { }
+
+    @Get('/whoami')
+    whoAmI(@Session() session: any) {
+        return this.usersService.findOne(session.userId)
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null
+    }
+
     @Post('/signup')
     async createUser(@Body() body: CreateUserDto, @Session() session: any) {
         const user = await this.authService.signUp(body.email, body.password);
