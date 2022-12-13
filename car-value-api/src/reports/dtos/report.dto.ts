7a7e3beac2685d0a408740b5d6025bddc34966c7
Matt Cook <mathew.cook@mastery.net>
2022-12-13 11:44:47 -0600

    setting up assocation not show user pass in res and a point to user via id

---
+++ car-value-api/src/reports/dtos/report.dto.ts
@@ -0,0 +1,32 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  long: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  mileage: number;
+
+  // Looks at the report entity, look at its user property and take the id and assign to this value
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
