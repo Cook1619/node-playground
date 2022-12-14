eed470e7b4baf4355944b0219b77d1879b4bc4a9
Matt Cook <mathew.cook@mastery.net>
2022-12-14 09:37:05 -0600

    setting up patch handler to approve/unapprove a report

--- car-value-api/src/reports/reports.service.ts
+++ car-value-api/src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/users.entity';
 import { Repository } from 'typeorm';
@@ -13,6 +13,15 @@ export class ReportsService {
     const report = this.repo.create(reportDto);
     // This sets up the association in the DB
     report.user = user;
+    return this.repo.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
+      throw new NotFoundException(`Report ${id} not found.`);
+    }
+    report.approved = approved;
     return this.repo.save(report);
   }
 }
